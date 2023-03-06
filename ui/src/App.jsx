c4b71e87d38c6cac513190f56c6ce62ebc240428
Anne L'Hôte <anne.lhote@gmail.com>
2023-03-06 15:38:38 +0100

    feat(load): Create Load component

--- ui/src/App.jsx
+++ ui/src/App.jsx
@@ -2,6 +2,8 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import './App.css'
 
+import Load from './load'
+
 function App() {
   const [count, setCount] = useState(0)
 
@@ -27,6 +29,7 @@ function App() {
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>
+      <Load />
     </div>
   )
 }
