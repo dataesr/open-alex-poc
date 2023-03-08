0e9682c5e5c7fdaf93c3c518edf073f3366ae16b
Mihoub Debache <debache.mihoub@gmail.com>
2023-03-08 10:31:53 +0100

    fix(graphs): update graphs

--- ui/src/components/publication-by-year/index.jsx
+++ ui/src/components/publication-by-year/index.jsx
@@ -1,47 +1,44 @@
 import React from "react";
-import data from "../../../data/huawei_france.json"
-import Highcharts from 'highcharts';
-import HighchartsReact from 'highcharts-react-official';
+import data from "../../../data/huawei_france.json";
+import Highcharts from "highcharts";
+import HighchartsReact from "highcharts-react-official";
 
 function PublicationByYear() {
-
-  let publicationsGroupedByYear = data?.results?.reduce(function (acc, publication) {
+  let publicationsGroupedByYear = data?.results?.reduce(function (
+    acc,
+    publication
+  ) {
     let year = publication.publication_year;
     if (!acc[year]) {
       acc[year] = [];
     }
     acc[year].push(publication);
     return acc;
-
-  }, {});
-  const years = Object.entries(publicationsGroupedByYear).map(([year, publications]) => ({
-    year: year,
-    y: publications.length,
-  }));
+  },
+  {});
+  const years = Object.entries(publicationsGroupedByYear).map(
+    ([year, publications]) => ({
+      year: year,
+      y: publications.length,
+    })
+  );
 
   const options = {
-    chart: { type: 'column' },
+    chart: { type: "column" },
     credits: { enabled: false },
     legend: { enabled: false },
-    series: [{ year: 'Nombre de publications', data: years }],
-    title: { text: 'Évolution des publications par année' },
-    xAxis: { title: { text: 'Année' } },
-    yAxis: { title: { text: 'Nombre de publications' } },
+    series: [{ name: "Nombre de publications", data: years }],
+    title: { text: "Évolution des publications par année" },
+    xAxis: {
+      title: { text: "Année" },
+      categories: Object.keys(publicationsGroupedByYear),
+    },
+    yAxis: { title: { text: "Nombre de publications" } },
   };
 
   return (
     <div>
-      <h1> Nombre total de publication : {data.results.length}</h1>
-      <div>
-        {years.map(({ year, y }) => (
-          <div key={year}>
-            <p>{year} : {y}</p>
-          </div>
-        ))}
-      </div>
-      <div>
-        <HighchartsReact highcharts={Highcharts} options={options} />
-      </div>
+      <HighchartsReact highcharts={Highcharts} options={options} />
     </div>
   );
 }
