94f46dee7add5d5efe6ebacd658404b844450799
eric <eric.jeangirard@recherche.gouv.fr>
2023-03-16 15:28:18 +0100

    footer

--- ui/src/layout/footer.jsx
+++ ui/src/layout/footer.jsx
@@ -4,14 +4,25 @@ import {
   FooterBodyItem,
   Link,
 } from '@dataesr/react-dsfr';
+import './footer.scss';
 
 export default function Footer() {
   return (
-    <FooterWrapper className="fr-mt-4w">
+    <FooterWrapper>
       <FooterBody description="">
         <FooterBodyItem>
+          <Link target="_blank" href="https://frenchopensciencemonitor.esr.gouv.fr">
+            French Open Science Monitor
+          </Link>
+        </FooterBodyItem>
+        <FooterBodyItem>
+          <Link target="_blank" href="https://scanr.enseignementsup-recherche.gouv.fr/">
+            scanR - explore the French scientific landscape
+          </Link>
+        </FooterBodyItem>
+        <FooterBodyItem>
           <Link target="_blank" href="https://github.com/dataesr/open-alex-poc">
-            Link to the open source of this app (Github)
+            Source code of this app (Github)
           </Link>
         </FooterBodyItem>
         <FooterBodyItem>
