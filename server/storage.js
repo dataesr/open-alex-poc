15b0ed1d42391b5be03b0b8b75cd5d630f239bae
folland87 <frederic@olland.me>
2023-06-23 11:06:20 +0200

    add cache expirancy

--- server/storage.js
+++ server/storage.js
@@ -7,6 +7,18 @@ import {
 
 import config from './config';
 
+const CACHE_DURATION_DAYS = 7;
+
+function shouldUseCache(lastModified) {
+  if (!lastModified) return false;
+  const now = new Date();
+  const cacheValidAfter = new Date(now.getFullYear(), now.getMonth(), now.getDate() - CACHE_DURATION_DAYS);
+  console.log(`Cache valid after ${cacheValidAfter}`);
+  console.log(`Last modified ${lastModified}`);
+  console.log(`Should use cache? ${lastModified > cacheValidAfter}`);
+  return lastModified > cacheValidAfter;
+}
+
 const { bucket, ...configS3 } = config.s3;
 export const s3 = new S3Client(configS3);
 
@@ -18,6 +30,9 @@ export default {
     ContentType: 'application/json',
     ACL: 'public-read',
   })),
-  exists: async (key) => s3.send(new HeadObjectCommand({ Bucket: bucket, Key: key })),
+  exists: async (key) => {
+    const head = await s3.send(new HeadObjectCommand({ Bucket: bucket, Key: key }))
+    return shouldUseCache(head?.LastModified);
+  },
   get: async (key) => s3.send(new GetObjectCommand({ Bucket: bucket, Key: key })),
 };
